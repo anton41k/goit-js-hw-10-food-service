8ea37c67af4fd5270368ba13eb41dc42d9ad14cb
anton41k <anton12petrenko@gmail.com>
2021-06-28 02:12:18 +0300

    hw10food

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
+import dataBaseFood from './menu.json';
+import eventsTemplaters from './templaters/food.hbs';
 import './sass/main.scss';
+
+
+const manuFoodUl = document.querySelector('ul.js-menu');
+manuFoodUl.innerHTML = eventsTemplaters(dataBaseFood);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const body = document.querySelector('body');
+const checkboxEl = document.querySelector('#theme-switch-toggle');
+
+onUpdateTheme()
+
+checkboxEl.addEventListener('change', onChangeCheckBox);
+
+function onUpdateTheme() {
+    const saveCheckBox = localStorage.getItem('isLightTheme');
+    if (saveCheckBox === 'true') {
+        checkboxEl.checked = true
+        onChangeTheme('LIGHT', 'DARK') 
+    }
+    else if (saveCheckBox === 'false' || !saveCheckBox){
+        checkboxEl.checked = false
+        onChangeTheme('DARK', 'LIGHT') 
+    }
+}
+
+
+function onChangeCheckBox(ev) {
+    const isChecked = ev.target.checked;
+    if (isChecked) {
+        onChangeTheme('LIGHT', 'DARK', isChecked);
+    }
+    else {
+        onChangeTheme('DARK', 'LIGHT', isChecked);
+    }
+}
+
+function onChangeTheme(removeCl, addCl, isLightTheme) {
+    body.classList.add(Theme[addCl]);
+    body.classList.remove(Theme[removeCl]);
+    if (isLightTheme != undefined) {
+        localStorage.setItem('isLightTheme', isLightTheme);
+    }
+}
